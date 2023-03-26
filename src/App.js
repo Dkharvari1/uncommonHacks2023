8a095c772f55021cb701008463717df5703dc95d
Joseph A. Mogavero <42627860+twinpeaksaurus@users.noreply.github.com>
2023-03-25 20:43:05 -0500

    added logic for song selection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { useRef, useEffect, useState } from 'react';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import * as faceapi from "face-api.js";
+import SongCard from "./components/songCard";
+
 
 function App() {
   const videoRef = useRef();
@@ -11,45 +15,55 @@ function App() {
     startVideo();
 
     videoRef && loadModels();
-
   }, []);
 
   const loadModels = () => {
     Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
     ]).then(() => {
       faceDetection();
-    })
+    });
   };
 
   const startVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
       .then((currentStream) => {
         videoRef.current.srcObject = currentStream;
       })
       .catch((err) => {
-        console.error(err)
+        console.error(err);
       });
-  }
+  };
 
   const faceDetection = async () => {
     setInterval(async () => {
-      const detections = await faceapi.detectSingleFace(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      const detections = await faceapi
+        .detectSingleFace(
+          videoRef.current,
+          new faceapi.TinyFaceDetectorOptions()
+        )
+        .withFaceLandmarks()
+        .withFaceExpressions();
 
-      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current);
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(
+        videoRef.current
+      );
       faceapi.matchDimensions(canvasRef.current, {
         width: 940,
         height: 650,
-      })
+      });
 
       const resized = faceapi.resizeResults(detections, {
         width: 940,
         height: 650,
       });
-      console.log("--------------------------------------------------------------------");
+      console.log(
+        "--------------------------------------------------------------------"
+      );
       console.log("Happy", detections.expressions.happy);
       console.log("Sad", detections.expressions.sad);
       console.log("Neutral", detections.expressions.neutral);
@@ -57,7 +71,9 @@ function App() {
       console.log("Surprised", detections.expressions.surprised);
       console.log("Fearful", detections.expressions.fearful);
       console.log("Disgusted", detections.expressions.disgusted);
-      console.log("--------------------------------------------------------------------");
+      console.log(
+        "--------------------------------------------------------------------"
+      );
       expressions.push([
         detections.expressions.angry,
         detections.expressions.happy,
@@ -65,26 +81,39 @@ function App() {
         detections.expressions.neutral,
         detections.expressions.surprised,
         detections.expressions.fearful,
-        detections.expressions.disgusted
-      ])
-      console.log((detections.expressions.angry + detections.expressions.happy + detections.expressions.sad + detections.expressions.neutral + detections.expressions.surprised + detections.expressions.fearful + detections.expressions.disgusted))
+        detections.expressions.disgusted,
+      ]);
+      console.log(
+        detections.expressions.angry +
+          detections.expressions.happy +
+          detections.expressions.sad +
+          detections.expressions.neutral +
+          detections.expressions.surprised +
+          detections.expressions.fearful +
+          detections.expressions.disgusted
+      );
       console.log(detections);
 
-      faceapi.draw.drawDetections(canvasRef.current, resized)
-      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)
-      faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
-
-    }, 1000)
-  }
+      faceapi.draw.drawDetections(canvasRef.current, resized);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+    }, 1000);
+  };
 
   return (
     <div className="app">
       <h1> AI FACE DETECTION</h1>
-      <div className='app__video'>
-        <video crossOrigin='anonymous' ref={videoRef} autoPlay ></video>
+      <div className="app__video">
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
       </div>
-      <canvas ref={canvasRef} width="940" height="650" className='app__canvas' />
-
+      <canvas
+        ref={canvasRef}
+        width="940"
+        height="650"
+        className="app__canvas"
+      />
+      <br/><br/>
+      <SongCard/>
     </div>
   );
 }
