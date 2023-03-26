d36a1e3d0e70bf954822e6d7a45b0446d573a5db
Joseph A. Mogavero <42627860+twinpeaksaurus@users.noreply.github.com>
2023-03-25 21:30:32 -0500

    playlist display table components created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import * as faceapi from "face-api.js";
-import SongCard from "./components/songCard";
+import SongCard from "./components/playlistTable/songCard";
 
 
 function App() {
